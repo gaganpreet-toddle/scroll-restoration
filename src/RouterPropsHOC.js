1e60f4c5877d8f3360335519b5d91c31fa6eff53
Jashandeep Singh <jashandeep@toddleapp.com>
2025-01-17 16:06:17 +0530

    feature: add filter change check logic in RouterPropsHOC

--- src/RouterPropsHOC.js
+++ src/RouterPropsHOC.js
@@ -8,29 +8,43 @@ function withRouterPropsContext(WrappedComponent) {
     const { prevRouterProps, currentRouterProps } =
       React.useContext(MyRouterPropsContext);
 
-    const isPopAction = currentRouterProps.location.action === "POP";
-    const isRestoreScroll =
-      currentRouterProps.location.state &&
-      currentRouterProps.location.state.restoreScroll === true;
+    const prevFilters = React.useRef(null);
 
-    // Logic to fetch new data or not
-    let fetchNew = true;
-    if (isPopAction) {
-      if (prevRouterProps === null) {
-        fetchNew = true; // Reload happened !
-      } else {
+    const fetchNewDataFunc = (filter) => {
+      const prev = JSON.stringify(prevFilters.current);
+      const current = JSON.stringify(filter);
+
+      const isPopAction = currentRouterProps.location.action === "POP";
+      const isRestoreScroll =
+        currentRouterProps.location.state &&
+        currentRouterProps.location.state.restoreScroll === true;
+
+      // Logic to fetch new data or not
+      let fetchNew = true;
+      if (isPopAction) {
+        if (prevRouterProps === null) {
+          fetchNew = true; // Reload happened !
+        } else {
+          fetchNew = false;
+        }
+      } else if (isRestoreScroll) {
         fetchNew = false;
       }
-    } else if (isRestoreScroll) {
-      fetchNew = false;
-    }
+
+      if (prevFilters.current !== null && prev !== current) {
+        fetchNew = true;
+      }
+
+      prevFilters.current = filter;
+      return fetchNew;
+    };
 
     return (
       <WrappedComponent
         {...props}
         prevRouterProps={prevRouterProps}
         currentRouterProps={currentRouterProps}
-        fetchNewData={fetchNew}
+        fetchNewData={fetchNewDataFunc}
       />
     );
   };
